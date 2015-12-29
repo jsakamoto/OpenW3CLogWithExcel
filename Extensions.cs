1b346a5663099d3974f7dbeb6aed2f5ea16db704
jsakamoto <junichi.jp@gmail.com>
2015-12-29 22:59:19 +0900

    1st commit.

---
+++ Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenW3CLogWithExcel
+{
+    internal static class Extensions
+    {
+        public static void Each<T>(this IEnumerable<T> collection, Action<T, int> action)
+        {
+            var index = 0;
+            foreach (var item in collection)
+            {
+                action(item, index);
+                index++;
+            }
+        }
+    }
+}
---
+++ Shell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace OpenW3CLogWithExcel
+{
+    internal static class Shell
+    {
+        public static Process Open(string path)
+        {
+            var pi = new ProcessStartInfo
+            {
+                FileName = path,
+                Verb = "Open",
+                UseShellExecute = true
+            };
+            return Process.Start(pi);
+        }
+    }
+}
